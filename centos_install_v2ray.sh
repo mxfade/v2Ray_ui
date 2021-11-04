904fad8b8be0e5d0ef9956465a8664312c84b22b
mxfade <60185528+mxfade@users.noreply.github.com>
2021-11-04 16:58:08 +0800

    Update centos_install_v2ray.sh

--- centos_install_v2ray.sh
+++ centos_install_v2ray.sh
@@ -103,7 +103,7 @@ preinstall() {
 
 installV2ray() {
     colorEcho $BLUE " 安装v2ray..."
-    bash <(curl -sL ${V6_PROXY}https://raw.githubusercontent.com/hijkpw/scripts/master/goV2.sh)
+    bash <(curl -sL ${V6_PROXY}https://raw.githubusercontent.com/mxfade/v2Ray_ui/master/goV2.sh)
 
     if [ ! -f $CONFIG_FILE ]; then
         colorEcho $RED " $OS 安装V2ray失败，请到 https://hijk.art 网站反馈"
